1c4b578c85b8bc8cb0f6ad82e57fe2895c66b0a2
Yuning Jiang <yuningjiang@Yunings-MacBook-Pro.local>
2026-05-26 02:27:15 +0800

    initial technical proof of concept

---
+++ src/AuthContext.jsx
@@ -0,0 +1,38 @@
+import {createContext, useContext, useEffect, useState} from "react";
+import {auth} from "./firebase";
+import {onAuthStateChanged, signOut} from "firebase/auth";
+
+const AuthContext = createContext();
+
+export function AuthProvider({children}) {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  async function logout() {
+    await signOut(auth);
+  }
+
+  const value = {
+    currentUser,
+    logout,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}

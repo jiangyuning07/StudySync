b4e0f6514313fcb392bed9248979efe9bb228ad4
Jiang Yuning <yuningjiang07@outlook.com>
2026-06-18 10:19:43 +0800

    Make page path and navbar link consistent for My Sessions

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ function App() {
           }
         />
         <Route
-          path="/sessions"
+          path="/all-sessions"
           element={
             <ProtectedRoute>
               <AllSessions />
